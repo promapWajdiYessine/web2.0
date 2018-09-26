669a0900eabb01080e1f2156be50e4999b4b5940
berrichewajdi@gmail.com <wberriche@monixo.com>
2018-09-26 23:13:59 +0200

    validation message

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(bodyParser.json());
 app.post('/',(req,res)=>{
   var email = req.body.email;
   var amount = req.body.amount;
+  if(amount <=1 ){
+    return_info = {};
+    return_info.error = true;
+    return_info.message = "the amount should be greater than 1";
+    return res.send(return_info);
+  }
   res.send({"amount": amount, "email": email});
 });
 
